16cb4d6d4ece9f9140d6469bc113a5eb0d96abb2
NikolayTkachenko <tkachenni@mail.ru>
2026-07-14 16:42:50 +0300

    update codes

--- computer_vision/read_video.py
+++ computer_vision/read_video.py
@@ -5,5 +5,10 @@ cap = cv2.VideoCapture("auto_fro_camera_short.mp4")
 while True:
     success, img = cap. read()
 
+    if not success:
+        break
+
     cv2.imshow("Video", img)
-    cv2.waitKey(1)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
